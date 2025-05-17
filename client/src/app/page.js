06b9a41ef205a1ad79e7760f05ef48d28022906c
dagmawib <dagmawibelayneh14@gmail.com>
2025-05-17 14:00:39 +0300

    style: update UI colors for product form and table components

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -38,12 +38,12 @@ export default function Page() {
   };
 
   return (
-    <div className="max-w-4xl mx-auto px-4 py-8 bg-white shadow-md rounded mt-2">
-      <h1 className="text-2xl font-bold mb-6">Add Product</h1>
+    <div className="max-w-4xl mx-auto px-4 py-8 bg-[#0C1825] shadow-md rounded mt-2">
+      <h1 className="text-2xl font-bold mb-6 text-white">Add Product</h1>
 
       <form
         onSubmit={handleSubmit}
-        className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8"
+        className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8 text-white"
       >
         <input
           type="text"
@@ -86,12 +86,12 @@ export default function Page() {
         />
         <button
           type="submit"
-          className="bg-blue-600 text-white rounded px-4 py-2 hover:bg-blue-700 md:col-span-2"
+          className="bg-[#C69950] text-[#0C1825] rounded px-4 py-2 hover:bg-[#c6c450] md:col-span-2"
         >
           Add Product
         </button>
       </form>
-      <h2 className="text-xl font-bold mb-4">Product List</h2>
+      <h2 className="text-xl font-bold mb-4 text-white">Product List</h2>
       <ProductTable products={products} />
     </div>
   );
