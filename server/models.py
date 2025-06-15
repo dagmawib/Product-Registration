381001c9fa39d5e57de63d0c50783cc13ac38560
MichaelBirhanuGmichael <michaelbirhanualem@gmail.com>
2025-06-15 19:11:11 +0300

    fix(models): change import of Base to absolute import

--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
 from sqlalchemy.orm import relationship
-from .database import Base # Changed back to relative import
+from database import Base # Changed to absolute import
 
 class Store(Base):
     __tablename__ = "stores"
